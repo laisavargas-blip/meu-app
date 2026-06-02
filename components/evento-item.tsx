385cfe0da7e7787d7517c6c893c350da50844df2
Lais Vargas <lais_a_vargas@estudante.sesisenai.org.br>
2026-06-02 15:25:06 -0300

    agr3

--- components/evento-item.tsx
+++ components/evento-item.tsx
@@ -19,7 +19,17 @@ export default function EventoItem() {
         <Image style={styles.imagem} source={{ uri: evento.imagem }} />
         <Text style={styles.titulo}>{evento.titulo}</Text>
         <Text style={styles.descricao}>{evento.descricao}</Text>
-        <Text style={styles.texto}> <i data-lucide="alarm-clock"></i>  Quando: {evento.data}</Text>
+        <Text style={styles.texto}><script>
+import { createIcons, alarmClock } from 'lucide';
+
+createIcons({
+  icons: {
+    alarmClock
+  }
+});
+</script>
+
+<i data-lucide="alarm-clock"></i>  Quando: {evento.data}</Text>
         <Text style={styles.texto}>Valor: R$ {evento.valor}</Text>
       </View>
       <View style={styles.reserva}>
